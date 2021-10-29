56621c1ab799803098301b3cbe13bc8e685cf5f1
Zarinna Dulnuan <zr.dulnuan@gmail.com>
2021-10-29 18:20:17 -0500

    Cookies

--- app.js
+++ app.js
@@ -32,35 +32,38 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-  console.log(req.header);
+  console.log(req.signedCookies);
 
   var authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401
-    return next(err)
-  }
-
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-  var username = auth[0];
-  var password = auth[1];
-
-  if (username === 'admin' && password === 'password') {
-    next();
-  }
-  else {
-    var err = new Error('You are not authenticated!');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401
-    return next(err)
+  if (!req.signedCookies.user) {
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+  
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401
+      return next(err)
+    }
+  
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  
+    var username = auth[0];
+    var password = auth[1];
+  
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', {signed: true})
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+  
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401
+      return next(err)
+    }
   }
 }
 

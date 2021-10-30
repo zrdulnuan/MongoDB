2e7f9069c776b69b531304b847a994aefba51bfe
Zarinna Dulnuan <zr.dulnuan@gmail.com>
2021-10-30 10:40:53 -0500

    Express Sessions Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,14 +34,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
   var authHeader = req.headers.authorization;
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
   
@@ -48,13 +58,13 @@ function auth(req, res, next) {
       return next(err)
     }
   
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
   
     var username = auth[0];
     var password = auth[1];
   
     if (username === 'admin' && password === 'password') {
-      res.cookie('user', 'admin', {signed: true})
+      req.session.user = 'admin';
       next();
     }
     else {
@@ -63,6 +73,17 @@ function auth(req, res, next) {
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401
       return next(err)
+    }
+  }
+  else {
+    if (req.session.user === 'admin') {
+      console.log('req.session: ', req.session);
+      next();
+    }
+    else {
+      var err = new Error ('You are not authenticated!');
+      err.status = 401;
+      next(err);
     }
   }
 }

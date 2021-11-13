3f25c195e96f4d0fb6c4fea13219dc4eb82f8685
Zarinna Dulnuan <zr.dulnuan@gmail.com>
2021-11-13 12:22:37 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '278360417409343',
+        clientSecret: 'cbc73df252f2577470fc32c14e2d995c'
+    }
 }
